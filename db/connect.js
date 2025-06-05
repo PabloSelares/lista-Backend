a01a6a5135b8d1718231b6f5058749fb0f228f86
Talles Oliveira Sobral <tallessr123@Gmail.com>
2025-06-05 10:40:03 -0300

    update connect.js
    foi atualizado o ip para subir o projeto

--- db/connect.js
+++ db/connect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = () => {
   return mongoose.connect(
-    "mongodb+srv://pabloselares:root@cluster0.hk5r4nv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    "mongodb+srv://talles:root@lista.bal4y7o.mongodb.net/?retryWrites=true&w=majority&appName=lista"
   )
   .then(() => console.log('Conectado ao MongoDB Atlas!'))
   .catch(err => console.log('Erro na conexão:', err));
